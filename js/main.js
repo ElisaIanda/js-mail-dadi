d566f4d04d39e2768d399d4db5c9080b5dc48cd5
ElisaIanda <elisaianda01@gmail.com>
2023-07-06 10:51:32 +0200

    Sistemato sistema di email

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const inputEmail = document.getElementById("input_email");
 const btnConfirm = document.getElementById("btn-confirm");
 const contactFormbg = document.getElementById("contact-form-bg")
 const confirmEmail = ["elisaianda01@gmail.com", "user@gmail.com", "pippo@gmail.com", "102@gmail.com", "pluto@gmail.com"];
-let emailFound = false;
+// let emailFound = false;
 
 btnConfirm.addEventListener("click", function () {
 
@@ -10,15 +10,15 @@ btnConfirm.addEventListener("click", function () {
     
 
     for (let i = 0; i < confirmEmail.length; i++) {
-
+        // emailFound = false;
         if (email === confirmEmail[i]) {
 
             console.log("Congratulazioni l'email inserita è corretta!")
-            emailFound = true;
+            i = confirmEmail.length;
             contactFormbg.style.backgroundColor = "#198754";
 
 
-        } else if (i !== confirmEmail[i] && emailFound === false) {
+        } else if (i === confirmEmail.length - 1) {
 
             console.log("Ci dispiace l'email inserita è sbagliata!");
             contactFormbg.style.backgroundColor = "#dc3545";
